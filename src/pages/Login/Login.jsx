a73b626173f255382bf70bac8e48866df68d9ff6
Javier Mauricio Gomez <javiermauriciogomez97@gmail.com>
2024-12-17 03:44:26 -0300

    Se agrega estructura de formulario de login y estilos en login.css para manejo de showpassword

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./login.css";
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
   return (
-    <div>Login</div>
-  )
+    <>
+      <div className="container">
+        <div className="row text-center mb-5 text-dark ">
+          <div className="col-sm-12">
+            <h2>Iniciar Sesion</h2>
+          </div>
+          <div className="col">
+            <form className="d-flex flex-column justify-content-center align-items-center bg-dark text-white">
+              <div className="mb-3">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  id="email"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password">Contraseña</label>
+                <div className="position-relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    className="form-control"
+                    name="password"
+                    id="password"
+                  />
+                  {showPassword ? (
+                    <i
+                      className="bi bi-eye-slash-fill position-absolute showPassword"
+                      onClick={() => setShowPassword(!showPassword)}
+                    ></i>
+                  ) : (
+                    <i
+                      className="bi bi-eye-fill position-absolute showPassword"
+                      onClick={() => setShowPassword(!showPassword)}
+                    ></i>
+                  )}
+                </div>
+              </div>
+              <div className="mb-3 d-grid">
+                <button className="btn btn-success">Sign in</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
