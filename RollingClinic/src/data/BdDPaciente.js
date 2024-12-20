6948a02e32568f8a7cf413e0b02277c02417ea90
jose maria perez <josema.perez.020@gmail.com>
2024-12-19 21:55:59 -0300

    modificar array

--- RollingClinic/src/data/BdDPaciente.js
+++ RollingClinic/src/data/BdDPaciente.js
@@ -30,83 +30,14 @@ export const pacientes = [
         
     },
     {
-        id: 3,
-        turno: "003",
-        nombre: "Carlos Gómez",
+        id: 4,
+        turno: "004",
+        nombre: "Carlos lopez",
         descripcion: "Dolor abdominal",
         detalles: "Dolor agudo en la parte baja del abdomen desde ayer.",
         estado: "en espera",
         resumenConsulta: "Se realizó palpación abdominal y se recomendó ultrasonido.",
         
     },
-    {
-        id: 3,
-        turno: "003",
-        nombre: "Carlos Gómez",
-        descripcion: "Dolor abdominal",
-        detalles: "Dolor agudo en la parte baja del abdomen desde ayer.",
-        estado: "en espera",
-        resumenConsulta: "Se realizó palpación abdominal y se recomendó ultrasonido.",
-        
-    },
-    {
-        id: 3,
-        turno: "003",
-        nombre: "Carlos Gómez",
-        descripcion: "Dolor abdominal",
-        detalles: "Dolor agudo en la parte baja del abdomen desde ayer.",
-        estado: "en espera",
-        resumenConsulta: "Se realizó palpación abdominal y se recomendó ultrasonido.",
-        
-    },
-    {
-        id: 2,
-        turno: "002",
-        nombre: "María López",
-        descripcion: "Fiebre y tos",
-        detalles: "Fiebre de 38.5°C acompañada de tos seca por 2 días.",
-        estado: "en espera",
-        resumenConsulta: "Pendiente de revisión médica.",
-       
-    },
-    {
-        id: 3,
-        turno: "003",
-        nombre: "Carlos Gómez",
-        descripcion: "Dolor abdominal",
-        detalles: "Dolor agudo en la parte baja del abdomen desde ayer.",
-        estado: "en espera",
-        resumenConsulta: "Se realizó palpación abdominal y se recomendó ultrasonido.",
-        
-    },
-    {
-        id: 3,
-        turno: "003",
-        nombre: "Carlos Gómez",
-        descripcion: "Dolor abdominal",
-        detalles: "Dolor agudo en la parte baja del abdomen desde ayer.",
-        estado: "en espera",
-        resumenConsulta: "Se realizó palpación abdominal y se recomendó ultrasonido.",
-        
-    },
-    {
-        id: 3,
-        turno: "003",
-        nombre: "Carlos Gómez",
-        descripcion: "Dolor abdominal",
-        detalles: "Dolor agudo en la parte baja del abdomen desde ayer.",
-        estado: "en espera",
-        resumenConsulta: "Se realizó palpación abdominal y se recomendó ultrasonido.",
-        
-    },
-    {
-        id: 3,
-        turno: "003",
-        nombre: "Carlos Gómez",
-        descripcion: "Dolor abdominal",
-        detalles: "Dolor agudo en la parte baja del abdomen desde ayer.",
-        estado: "en espera",
-        resumenConsulta: "Se realizó palpación abdominal y se recomendó ultrasonido.",
-        
-    },
+    
 ];
