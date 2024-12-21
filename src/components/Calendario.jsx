cc0c52b58b5d0d5ebf830ff0227d82cd2a8ee116
naahuel-11 <nahuelromano911@gmail.com>
2024-12-21 00:04:30 -0300

    se creo el array de objetos doctores

--- src/components/Calendario.jsx
+++ src/components/Calendario.jsx
@@ -5,6 +5,49 @@ function Calendario() {
     const [mes, setMes] = useState(null);
     const [diaSeleccionado, setDiaSeleccionado] = useState(null);
     const [dias, setDias] = useState([]);
+
+    const [doctores, setDoctores] = useState([
+        {
+          idUnico: uuidv4(),
+          nombre: "Dr. Juan Pérez",
+          especialidad: "Cardiología",
+          horarios: {
+            mañana: ["09:00 AM", "10:00 AM", "11:00 AM", "12:00 AM"],
+            tarde: ["15:00 PM", "16:00 PM", "17:00 PM", "18:00 AM"],
+          },
+          turnosReservados: [],
+        },
+        {
+          idUnico: uuidv4(),
+          nombre: "Dra. María López",
+          especialidad: "Dermatología",
+          horarios: {
+            mañana: ["09:00 AM", "10:00 AM", "11:00 AM", "12:00 AM"],
+            tarde: ["15:00 PM", "16:00 PM", "17:00 PM", "18:00 AM"],
+          },
+          turnosReservados: [],
+        },
+        {
+          idUnico: uuidv4(),
+          nombre: "Dr. Pedro Sánchez",
+          especialidad: "Pediatría",
+          horarios: {
+            mañana: ["09:00 AM", "10:00 AM", "11:00 AM", "12:00 AM"],
+            tarde: ["15:00 PM", "16:00 PM", "17:00 PM", "18:00 AM"],
+          },
+          turnosReservados: [],
+        },
+        {
+          idUnico: uuidv4(),
+          nombre: "Dr. Pedro Sánchez",
+          especialidad: "Pediatría",
+          horarios: {
+            mañana: ["09:00 AM", "10:00 AM", "11:00 AM", "12:00 AM"],
+            tarde: ["15:00 PM", "16:00 PM", "17:00 PM", "18:00 AM"],
+          },
+          turnosReservados: [],
+        },
+      ]);
 }
 
 export default Calendario
