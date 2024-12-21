19b4834d672bf8207cdaf3b52bd61623508d46ac
naahuel-11 <nahuelromano911@gmail.com>
2024-12-21 00:02:13 -0300

    se instalo sweetaler y uuid, a su vez se creo el componente calendario y se crearon las variables con sus estados

---
+++ src/components/Calendario.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Calendario() {
+    const [anio, setAnio] = useState(null);
+    const [mes, setMes] = useState(null);
+    const [diaSeleccionado, setDiaSeleccionado] = useState(null);
+    const [dias, setDias] = useState([]);
+}
+
+export default Calendario
