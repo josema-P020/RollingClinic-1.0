7d778c9fdcff297ae82315f4dfd1cc11f5d199d3
naahuel-11 <nahuelromano911@gmail.com>
2024-12-20 23:02:11 -0300

    se creo la estructura del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="row pt-md-4 text-center footer-style">
+      <div className="col-sm-12 col-md-4 mb-3 mb-md-0">
+        <a className="navbar-brand" href="#">
+          <i className="fa fa-ravelry" aria-hidden="true">
+            CODER
+          </i>
+        </a>
+      </div>
+
+      <div className="col-sm-12 col-md-4">
+        <h5 className="">Soporte</h5>
+        <ul className="ul-style">
+          <li>
+            <a href="#">LISTA 2</a>
+          </li>
+          <li>
+            <a href="#">LISTA 2</a>
+          </li>
+          <li>
+            <a href="#">LISTA 2</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="col-sm-12 col-md-4">
+        <h5 className="">Contáctanos</h5>
+        <ul className="ul-style">
+          <li>
+            <a href="#">LISTA 2</a>
+          </li>
+          <li>
+            <a href="#">LISTA 2</a>
+          </li>
+          <li>
+            <a href="#">LISTA 2</a>
+          </li>
+        </ul>
+      </div>
+      <p className="text-center my-0 mt-2">
+        &copy; 2024. Todos los derechos reservados
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
