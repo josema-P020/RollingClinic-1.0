e1ca1f48bc8f35c76a72824ec0ae61caa0f9655e
Javier Mauricio Gomez <javiermauriciogomez97@gmail.com>
2024-12-22 05:58:47 -0300

    Se corrigen rutas, se asignan estilos de pagina de registro de paciente y medico en 2 forms diferente

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Login from './pages/Login/Login'
 import Calendario from "./components/Calendario";
+import RegisterDoctor from "./pages/Register/RegisterDoctor";
+import RegisterPatient from "./pages/Register/RegisterPatient";
 // import './App.css'
 
 function App() {
@@ -11,7 +13,8 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login/>}/>
-        {/* <Route path="/containerBtn" element={<ContainerBtn/>}/> */}
+        <Route path="/registerDoctor" element={<RegisterDoctor/>}/>
+        <Route path="/registerPatient" element={<RegisterPatient/>}/>
       </Routes>
       </BrowserRouter>
     </>
