6ebf20ea9fc9cba173bc4e33a59d6a86a97eb601
jose maria perez <josema.perez.020@gmail.com>
2024-12-21 23:37:53 -0300

    merge de home

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,23 @@ import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import RegisterPatient from "./pages/Register/RegisterPatient"
 import RegisterDoctor from "./pages/Register/RegisterDoctor"
-// import ContainerBtn from "./pages/ContainerLoginBtn/ContainerBtn"
+import HomeScreen from "./pages/HomeScreen"
 import "./App.css"
 function App() {
-  
+
   return (
-    <>
+    <> 
+        
+
       <BrowserRouter>
-      <Routes>
-<Route path="/RegisterPatient" element={<RegisterPatient/>}/>
-<Route path="/RegisterDoctor" element={<RegisterDoctor/>}/>
-        {/* <Route path="/login" element={<Login/>}/> */}
-        {/* <Route path="/containerBtn" element={<ContainerBtn/>}/> */}
-      </Routes>
+        <Routes>
+          <Route path="/" element={<HomeScreen/>} />
+          <Route path="/RegisterPatient" element={<RegisterPatient />} />
+          <Route path="/RegisterDoctor" element={<RegisterDoctor />} />
+          {/* <Route path="/login" element={<Login/>}/> */}
+        </Routes>
       </BrowserRouter>
-      
+
     </>
   )
 }
