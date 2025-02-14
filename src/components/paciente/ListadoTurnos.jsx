350a3d9abfc78110861e432a0492b4042646a66b
Jose Alvarez Dominguez <josealvarezdominguez00@gmail.com>
2025-02-13 23:45:39 -0300

    se realizo la lectura del localStorage y se renderizan los turnos correctamente

--- src/components/paciente/ListadoTurnos.jsx
+++ src/components/paciente/ListadoTurnos.jsx
@@ -1,16 +1,11 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 
 function ListadoTurnos() {
   const [info, setInfo] = useState(() => {
-    const turnosGuardados = localStorage.getItem("reservasTurnos");
     const loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
-    if (!turnosGuardados || !loggedInUser) return [];
-    const reservasTurnos = JSON.parse(turnosGuardados);
 
-    const turnosUsuario = reservasTurnos.filter((turno) => {
-      return turno.paciente && turno.paciente.id === loggedInUser.id;
-    });
-    return turnosUsuario;
+    if (!loggedInUser) return []; // Si no hay usuario, devuelve un array vacío
+    return Array.isArray(loggedInUser) ? loggedInUser : [loggedInUser];
   });
 
   return (
@@ -18,68 +13,35 @@ function ListadoTurnos() {
       <div className="text-center flex-grow-1">
         <h2 className="fw-bold pb-3">Mis Turnos</h2>
         <div className="table-responsive">
-          <table className="table table-hover ">
+          <table className="table table-hover">
             <thead>
               <tr>
-                <th className="text-start text-nowrap">Doctor / Doctora</th>
-                <th className="text-start text-nowrap">Fecha</th>
-                <th className="text-start text-nowrap">Hora</th>
+                <th className="text-center text-nowrap">Doctor / Doctora</th>
+                <th className="text-center text-nowrap">Fecha</th>
+                <th className="text-center text-nowrap">Hora</th>
               </tr>
             </thead>
             <tbody>
               {info.length === 0 ? (
-                <td colSpan="3" className="fw-bold py-2">
-                  No hay turnos aun
-                </td>
+                <tr>
+                  <td colSpan="3" className="fw-bold py-2">
+                    No hay turnos aún
+                  </td>
+                </tr>
               ) : (
-                info.map((turno) => (
-                  <tr key={turno.idUnico}>
-                    <td>
-                      <div>
-                        <div className="d-flex align-items-center">
-                          <span className="ms-2">{turno.doctor}</span>
-                        </div>
-                      </div>
-                    </td>
-                    <td className="align-middle text-start">{turno.fecha}</td>
-                    <td className="align-middle text-start">{turno.horario}</td>
-                  </tr>
-                ))
+                info.map((turno, key) =>
+                  turno.turnos.map((turn, index) => (
+                    <tr key={`${key}-${index}`}>
+                      <td>{turn.doctor}</td>
+                      <td>{turn.fecha}</td>
+                      <td>{turn.hora}</td>
+                    </tr>
+                  ))
+                )
               )}
             </tbody>
           </table>
         </div>
-      </div>
-      <div className="d-flex justify-content-center">
-        <nav aria-label="paginacion">
-          <ul className="pagination ">
-            <li className="page-item">
-              <a className="page-link disabled" href="#">
-                Anterior
-              </a>
-            </li>
-            <li className="page-item active ">
-              <a className="page-link" href="#">
-                1
-              </a>
-            </li>
-            <li className="page-item">
-              <a className="page-link disabled" href="#">
-                2
-              </a>
-            </li>
-            <li className="page-item">
-              <a className="page-link disabled" href="#">
-                3
-              </a>
-            </li>
-            <li className="page-item">
-              <a className="page-link disabled" href="#">
-                Siguiente
-              </a>
-            </li>
-          </ul>
-        </nav>
       </div>
     </div>
   );
